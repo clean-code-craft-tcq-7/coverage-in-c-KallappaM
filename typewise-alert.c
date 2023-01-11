779057d39acf8258e7043714bd5e228e67765ffb
KallappaM <115877818+KallappaM@users.noreply.github.com>
2023-01-11 19:55:20 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -34,29 +34,3 @@ void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double
   
 }
 
-void sendToController(BreachType breachType) 
-{
-  const unsigned short header = 0xfeed;
-  printf("%x : %x\n", header, breachType);
-   Tempflag.FlagTypeController = USED;
-}
-
-void sendToEmail(BreachType breachType) 
-{
-  Tempflag.FlagTypeMail = USED;
-  const char* recepient = "a.b@c.com";
-  if(breachType == TOO_LOW)
-  {
-    printf("To: %s\n", recepient);
-    printf("Hi, the temperature is too low\n");
-    Tempflag.FlagLowTemp = TEMPLOW;
-  }
-
-  if(breachType == TOO_HIGH)
-  {
-     printf("To: %s\n", recepient);
-     printf("Hi, the temperature is too high\n");
-    Tempflag.FlagHighTemp = TEMPHIGH;
-  }
-
-}
